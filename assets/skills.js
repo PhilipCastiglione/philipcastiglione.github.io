168e979ca79e65926a0d1255217f2ab542df7064
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-31 17:19:25 +1000

    added skills js data

---
+++ assets/skills.js
@@ -0,0 +1,378 @@
+var SKILLS = {
+  "Artificial Intelligence": [
+  {
+    "name": "Neural Networks",
+    "current": 0,
+    "goal": 5
+  }
+  ],
+  "Back End Web Development": [
+    {
+      "name": "Internet Protocols, Request Structure & Routing",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "Node.js",
+      "current": 0,
+      "goal": 1,
+      "comment": "With some basic express, I guess this is also npm and FE tooling..."
+    },
+    {
+      "name": "Ruby on Rails",
+      "current": 3,
+      "goal": 4
+    },
+    {
+      "name": "Sinatra",
+      "current": 1,
+      "goal": 1
+    },
+    {
+      "name": "Web/Application Security",
+      "current": 1,
+      "goal": 2
+    }
+  ],
+  "Computer Hardware": [
+    {
+      "name": "Desktop PC Components",
+      "current": 1,
+      "goal": 2,
+      "comment": "ie. motherboard, cpu (ALU, local/cached memory, control unit), ram, hard drive/ssd, graphics/video card, PSU, network/wireless, optical drive, input/output peripherals, heat distribution, cable management"
+    },
+    {
+      "name": "High Powered/Specialized Computing",
+      "current": 0,
+      "goal": 2
+    }
+  ],
+  "Computer Science": [
+    {
+      "name": "Algorithms",
+      "current": 1,
+      "goal": 4
+    },
+    {
+      "name": "Computer Architecture",
+      "current": 0,
+      "goal": 2
+    },
+    {
+      "name": "Concurrent, Parallel & Distributed Systems",
+      "current": 0,
+      "goal": 3
+    },
+    {
+      "name": "Data Structures",
+      "current": 1,
+      "goal": 4
+    },
+    {
+      "name": "Logic for Computation",
+      "current": 1,
+      "goal": 4
+    }
+  ],
+  "Data Management": [
+    {
+      "name": "NoSQL",
+      "current": 0,
+      "goal": 2
+    },
+    {
+      "name": "RDBMS Theory & Application",
+      "current": 1,
+      "goal": 3
+    },
+    {
+      "name": "Redis",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "SQL",
+      "current": 1,
+      "goal": 2
+    }
+  ],
+  "Dev & Sys Ops": [
+    {
+      "name": "AWS",
+      "current": 0,
+      "goal": 2,
+      "comment": "EC2, EBS, ElasticBeanstalk, RDS, Lambda, S3, Glacier, Auto Scaling, Load Balancing, ElastiCache, Cloudfront"
+    },
+    {
+      "name": "Capistrano",
+      "current": 1,
+      "goal": 0
+    },
+    {
+      "name": "CI/CD",
+      "current": 1,
+      "goal": 2,
+      "comment": "Continuous Integration, Delivery & Deployment"
+    },
+    {
+      "name": "Docker",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "Git",
+      "current": 3,
+      "goal": 3
+    },
+    {
+      "name": "Heroku",
+      "current": 2,
+      "goal": 2
+    },
+    {
+      "name": "Linux",
+      "current": 1,
+      "goal": 2,
+      "comment": "familiarity with Debian (use Ubuntu), Fedora (played with CentOS), SUSE (played with openSUSE)"
+    },
+    {
+      "name": "Mac OS",
+      "current": 2,
+      "goal": 2
+    },
+    {
+      "name": "Networking",
+      "current": 0,
+      "goal": 2
+    },
+    {
+      "name": "Server/Sys Admin",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "Shell Scripting",
+      "current": 1,
+      "goal": 3
+    },
+    {
+      "name": "Windows",
+      "current": 1,
+      "goal": 1
+    }
+  ],
+  "Front End Web Development": [
+    {
+      "name": "Browsers",
+      "current": 2,
+      "goal": 2
+    },
+    {
+      "name": "CSS/Sass",
+      "current": 2,
+      "goal": 1
+    },
+    {
+      "name": "Ember",
+      "current": 2,
+      "goal": 0
+    },
+    {
+      "name": "HTML",
+      "current": 2,
+      "goal": 2
+    },
+    {
+      "name": "Modern Web Development",
+      "current": 2,
+      "goal": 1,
+      "comment": "Cross browser, cross platform design, accessibility..."
+    },
+    {
+      "name": "React",
+      "current": 1,
+      "goal": 2,
+      "comment": "with redux"
+    }
+  ],
+  "Languages": [
+    {
+      "name": "C",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "C++",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Clojure",
+      "current": 1,
+      "goal": 1
+    },
+    {
+      "name": "Elixir",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Forth",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Haskell",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Java",
+      "current": 1,
+      "goal": 1
+    },
+    {
+      "name": "JavaScript",
+      "current": 2,
+      "goal": 2,
+      "comment": "ES6"
+    },
+    {
+      "name": "Lisp",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Lua",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Prolog",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Python",
+      "current": 1,
+      "goal": 2
+    },
+    {
+      "name": "R",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Ruby",
+      "current": 3,
+      "goal": 4
+    },
+    {
+      "name": "Rust",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Scala",
+      "current": 0,
+      "goal": 1
+    },
+    {
+      "name": "Smalltalk",
+      "current": 0,
+      "goal": 1
+    }
+  ],
+  "Machine Learning": [
+    {
+      "name": "Reinforcement Learning",
+      "current": 0,
+      "goal": 5
+    }
+  ],
+  "Mathematics": [
+    {
+      "name": "Linear Algebra",
+      "current": 1,
+      "goal": 3
+    },
+    {
+      "name": "Statistics & Probability",
+      "current": 1,
+      "goal": 3
+    }
+  ],
+  "Native Phone": [
+    {
+      "name": "Cordova",
+      "current": 1,
+      "goal": 0
+    },
+    {
+      "name": "Apple Development Process",
+      "current": 1,
+      "goal": 0
+    }
+  ],
+  "Software Development": [
+    {
+      "name": "API Design",
+      "current": 2,
+      "goal": 3
+    },
+    {
+      "name": "Design Patterns",
+      "current": 1,
+      "goal": 3
+    },
+    {
+      "name": "Development Methodologies",
+      "current": 2,
+      "goal": 3,
+      "comment": "Tools and systems ranging from Agile development methodologies to kanban tools and open source PR workflow"
+    },
+    {
+      "name": "Functional Programming",
+      "current": 1,
+      "goal": 4
+    },
+    {
+      "name": "Object Oriented Programming",
+      "current": 2,
+      "goal": 4
+    },
+    {
+      "name": "Requirements Capture",
+      "current": 3,
+      "goal": 3
+    },
+    {
+      "name": "Solution Design",
+      "current": 2,
+      "goal": 4
+    },
+    {
+      "name": "System Architecture",
+      "current": 1,
+      "goal": 3
+    }
+  ],
+  "Testing": [
+    {
+      "name": "RSpec",
+      "current": 2,
+      "goal": 3
+    },
+    {
+      "name": "TDD/BDD",
+      "current": 2,
+      "goal": 3,
+      "comment": "Test Driven Design/Behaviour Driven Design"
+    },
+    {
+      "name": "Testing Techniques & Strategy",
+      "current": 2,
+      "goal": 3
+    }
+  ]
+};
