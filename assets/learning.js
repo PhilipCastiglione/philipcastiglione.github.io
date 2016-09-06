3b277da0aedbfd33d2796c9e77f3bf24038a382c
Philip Castiglione <philipcastiglione@gmail.com>
2016-09-06 20:50:15 +1000

    added learning asset

---
+++ assets/learning.js
@@ -0,0 +1,102 @@
+var LEARNING = {
+  "Completed": [
+    {
+      "name": "LPTHW",
+      "description": "Learn Python The Hard Way, by Zed Shaw at learncodethehardway.com"
+    },
+    {
+      "name": "Web Development Immersive",
+      "description": "A 12 week intense bootcamp/accelerator course by General Assembly"
+    },
+    {
+      "name": "Intro to Java 1",
+      "description": "by HKUST, delivered via edX"
+    },
+    {
+      "name": "Intro to Java 2",
+      "description": "by HKUST, delivered via edX"
+    },
+    {
+      "name": "Intro to Python For Data Science",
+      "description": "by Microsoft, delivered via edX"
+    },
+    {
+      "name": "Practical Object-Oriented Design in Ruby",
+      "description": "by Sandi Metz"
+    },
+    {
+      "name": "Metaprogramming Ruby",
+      "description": "by Paolo Perotta"
+    },
+    {
+      "name": "Everyday Rails Testing With RSpec",
+      "description": "by Aaron Sumner"
+    },
+    {
+      "name": "React Training Videos",
+      "description": "A series of training videos on egghead.io"
+    },
+    {
+      "name": "Redux Training Videos",
+      "description": "A series of training videos on egghead.io delivered by the creator of Redux Dan Abramov"
+    }
+  ],
+  "Underway": [
+    {
+      "name": "LCTHW",
+      "description": "Learn C The Hard Way, by Zed Shaw at learncodethehardway.com"
+      "progress": "18/53"
+    },
+    {
+      "name": "Intro to Linux",
+      "description": "by the Linux Foundation, delivered via edX"
+      "progress": "16/18"
+    },
+    {
+      "name": "CS50 Intro to Computer Science",
+      "description": "by Harvard, delivered via edX",
+      "progress": "3/13"
+    },
+    {
+      "name": "Hacking: The Art Of Exploitation",
+      "description": "by Jon Erickson",
+      "progress": "83/492"
+    },
+    {
+      "name": "Intro to R Programming",
+      "description": "Learn R in R with Swirl",
+      "progress": "9/15"
+    },
+    {
+      "name": "PH525.1x Statistics and R",
+      "description": "by Harvard, delivered via edX",
+      "progress": "0/4"
+    }
+  ],
+  "On Hold": [
+    {
+      "name": "Signals to Packets Part 1",
+      "description": "by HKUST, delivered via edX",
+      "progress": "9/15"
+    },
+    {
+      "name": "Clean Code",
+      "description": "by Uncle Bob",
+      "progress": "135/462"
+    }
+  ]
+  "Planned": [
+    {
+      "name": "Signals to Packets Part 2",
+      "description": "by HKUST, delivered via edX"
+    },
+    {
+      "name": "Signals to Packets Part 3",
+      "description": "by HKUST, delivered via edX"
+    },
+    {
+      "name": "Advanced Redux Training Videos",
+      "description": "A follow on advanced series of training videos on egghead.io delivered by the creator of Redux Dan Abramov"
+    }
+  ]
+};
