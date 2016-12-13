8ebd587c35e7b01e6f90c18b0dbc1b92f3f6caf9
Philip Castiglione <philipcastiglione@gmail.com>
2016-12-13 18:25:19 +1100

    learning

--- assets/learning.js
+++ assets/learning.js
@@ -82,7 +82,7 @@ var LEARNING = {
     {
       "name": "GeometryX: Introduction To Geometry",
       "description": "by SchoolYourself, delivered via edX",
-      "progress": "40%"
+      "progress": "46%"
     },
     {
       "name": "Haskell Programming",
