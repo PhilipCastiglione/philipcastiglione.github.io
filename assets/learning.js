ae5e8d9278e36384230c767f25bf9785ac7b2ccf
Philip Castiglione <philipcastiglione@gmail.com>
2017-01-19 09:29:24 +1100

    learning

--- assets/learning.js
+++ assets/learning.js
@@ -93,7 +93,7 @@ var LEARNING = {
     {
       "name": "A Field Guide To Lies And Statistics",
       "description": "by Daniel Levitin",
-      "progress": "112/292"
+      "progress": "142/292"
     },
     {
       "name": "Dive Into Python 3",
