0df91e19f6706d82ecdc2ebe76c42fe017ecc0b0
Philip Castiglione <philipcastiglione@gmail.com>
2017-01-19 15:37:58 +1100

    learning

--- assets/learning.js
+++ assets/learning.js
@@ -98,7 +98,7 @@ var LEARNING = {
     {
       "name": "Dive Into Python 3",
       "description": "by Mark Pilgrim at http://www.diveintopython3.net",
-      "progress": "5/22"
+      "progress": "6/22"
     },
     {
       "name": "LCTHW",
